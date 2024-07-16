266eb2acbad4ee969e5a22f4addbebbfc2a0652a
SelfShrimp <daniil.selyutin@gmail.com>
2024-07-17 01:33:48 +0300

    display the top list

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        string url = "https://store.steampowered.com/charts/topselling/RU";
+        string url = "https://store.steampowered.com/search/?hidef2p=1&filter=topsellers&ndl=1";
         var httpClient = new HttpClient();
 
         var response = await httpClient.GetStringAsync(url);
@@ -12,5 +12,13 @@ class Program
         var htmlDoc = new HtmlDocument();
         htmlDoc.LoadHtml(response);
 
+        var topSellers = htmlDoc.DocumentNode.SelectSingleNode("//div[@data-panel]");
+        for (int i = 0; i < 10; i++)
+        {
+            var gameName = topSellers.SelectNodes("//span[@class='title']")[i].InnerText;
+            var gamePrice = topSellers.SelectNodes("//div[@class='discount_final_price']")[i].InnerText;
+            Console.WriteLine($"Место: {i+1}. {gameName}. Цена: {gamePrice}"); 
+        }
+        Console.WriteLine();
     }
 }
