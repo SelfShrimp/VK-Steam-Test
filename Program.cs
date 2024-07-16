3eeb3a6be7931b4e6677b8f3bd056fd4112cc279
SelfShrimp <daniil.selyutin@gmail.com>
2024-07-16 17:36:21 +0300

    add a request to the sales leaders

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HtmlAgilityPack;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "https://store.steampowered.com/charts/topselling/RU";
+        var httpClient = new HttpClient();
+
+        var response = await httpClient.GetStringAsync(url);
+
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(response);
+
+    }
+}
