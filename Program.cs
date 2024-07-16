557a5ca2549367f9f8c14184786a2774c0d40dac
SelfShrimp <daniil.selyutin@gmail.com>
2024-07-17 01:43:38 +0300

    add exception checking

--- Program.cs
+++ Program.cs
@@ -4,21 +4,31 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        string url = "https://store.steampowered.com/search/?hidef2p=1&filter=topsellers&ndl=1";
-        var httpClient = new HttpClient();
-
-        var response = await httpClient.GetStringAsync(url);
-
-        var htmlDoc = new HtmlDocument();
-        htmlDoc.LoadHtml(response);
-
-        var topSellers = htmlDoc.DocumentNode.SelectSingleNode("//div[@data-panel]");
-        for (int i = 0; i < 10; i++)
+        try
         {
-            var gameName = topSellers.SelectNodes("//span[@class='title']")[i].InnerText;
-            var gamePrice = topSellers.SelectNodes("//div[@class='discount_final_price']")[i].InnerText;
-            Console.WriteLine($"Место: {i+1}. {gameName}. Цена: {gamePrice}"); 
+            string url = "https://store.steampowered.com/search/?hidef2p=1&filter=topsellers&ndl=1";
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetStringAsync(url);
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(response);
+
+            var topSellers = htmlDoc.DocumentNode.SelectSingleNode("//div[@data-panel]");
+            for (int i = 0; i < 10; i++)
+            {
+                var gameName = topSellers.SelectNodes("//span[@class='title']")[i].InnerText;
+                var gamePrice = topSellers.SelectNodes("//div[@class='discount_final_price']")[i].InnerText;
+                Console.WriteLine($"Место: {i + 1}. {gameName}. Цена: {gamePrice}");
+            }
         }
-        Console.WriteLine();
+        catch (HttpRequestException httpEx)
+        {
+            Console.WriteLine($"Ошибка при выполнении HTTP-запроса: {httpEx.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
     }
 }
